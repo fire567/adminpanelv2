ee0b111915a322d6f74e9a66f1b9a05c69b66e7b
fire567 <dante.nero.95@gmail.com>
2022-05-11 12:54:20 +0300

    fixed

---
+++ src/Component/MainRouts/MainRouts.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import LoginPage from '../../Pages/LoginPage/LoginPage';
+import MainPage from '../../Pages/MainPage/MainPage';
+import { Route } from 'react-router-dom';
+
+const MainRouts = () => {
+  return (
+    <>
+      <Route path='/' component={LoginPage} exact />
+      <Route path='/main-page/:name' component={MainPage} exact />
+    </>
+  );
+};
+
+export default MainRouts;
